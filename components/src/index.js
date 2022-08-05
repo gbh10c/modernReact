87f3ba30aaa44174814505fd35ef99174dc0e260
Gordon Hicken <gordon.hicken@gmail.com>
2022-08-05 10:46:47 -0400

    Added reuseable Approval Card

--- components/src/index.js
+++ components/src/index.js
@@ -1,30 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { faker } from '@faker-js/faker';
-
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
   return (
     <div className='ui container comments'>
-      <CommentDetail
-        author='Sam'
-        timeAgo='Today at 4:45 PM'
-        avatar={faker.image.avatar()}
-        text='Nice post!'
-      />
-      <CommentDetail
-        author='Pat'
-        timeAgo='Today at 9:12 AM'
-        avatar={faker.image.avatar()}
-        text='Very cool!'
-      />
-      <CommentDetail
-        author='Chris'
-        timeAgo='Yesterday at 7:58 PM'
-        avatar={faker.image.avatar()}
-        text='No way!'
-      />
+      <ApprovalCard>
+        <CommentDetail
+          author='Sam'
+          timeAgo='Today at 4:45 PM'
+          avatar={faker.image.avatar()}
+          text='Nice post!'
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author='Pat'
+          timeAgo='Today at 9:12 AM'
+          avatar={faker.image.avatar()}
+          text='Very cool!'
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author='Chris'
+          timeAgo='Yesterday at 7:58 PM'
+          avatar={faker.image.avatar()}
+          text='No way!'
+        />
+      </ApprovalCard>
     </div>
   );
 };
