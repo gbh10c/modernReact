38faa1b7ff87e5cee993b9d22d232695198a1af5
Gordon Hicken <gordon.hicken@gmail.com>
2022-08-06 10:22:50 -0400

    Accessed geolocation with setState

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -3,18 +3,28 @@ import ReactDOM from 'react-dom';
 
 class App extends React.Component {
   constructor(props) {
-        super(props);
-  }
+    super(props);
 
+    // THIS IS THE ONLY DIRECT ASSIGNMENT TO this.state
+    // every other assignment should be with this.setState (see below)
+    this.state = { lat: null };
+
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        // setState called to update!
+        this.setState({ lat: position.coords.latitude });
+
+        // THIS DOES NOT WORK:
+          // this.state.lat = position.coords.latitude
+        // NEVER CREATE A DIRECT ASSIGNMENT TO A STATE OBJECT!!!
+      },
+      (err) => console.log(err)
+    );
+  }
 
   // React says we have to define render!
   render() {
-    window.navigator.geolocation.getCurrentPosition(
-      (position) => console.log(position),
-      (err) => console.log(err)
-    );
-
-    return <div>Latitude: </div>;
+    return <div>Latitude: {this.state.lat}</div>;
   }
 }
 
