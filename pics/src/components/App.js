537d56fa47d08dab1cf473b9290b1a7048d7c92b
Gordon Hicken <gordon.hicken@gmail.com>
2022-08-11 20:06:03 -0400

    Handling onSearchSubmit request with async/await

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -3,20 +3,17 @@ import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    axios
-      .get('https://api.unsplash.com/search/photos', {
-        params: {
-          query: term,
-        },
-        headers: {
-          Authorization:
-            'Client-ID kduC508uOJCSOAUr83uMzXpb5MK2-VURk268on246FU',
-        },
-      })
-      .then((response) => {
-        console.log(response.data.results);
-      });
+  async onSearchSubmit(term) {
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
+      params: {
+        query: term,
+      },
+      headers: {
+        Authorization: 'Client-ID kduC508uOJCSOAUr83uMzXpb5MK2-VURk268on246FU',
+      },
+    });
+
+    console.log(response.data.results);
   }
 
   render() {
