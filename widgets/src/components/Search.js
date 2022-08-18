cf7d251e7e434e8b9c35c268bdf77ea0450d8eb1
Gordon Hicken <gordon.hicken@gmail.com>
2022-08-18 09:55:47 -0400

    Set up Search component

---
+++ widgets/src/components/Search.js
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+
+const Search = () => {
+  const [term, setTerm] = useState('');
+
+  return (
+    <div>
+      <div className='ui form'>
+        <div className='field'>
+          <label>Enter Search Term</label>
+          <input
+            value={term}
+            onChange={(e) => setTerm(e.target.value)}
+            type='text'
+            className='input'
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Search;
