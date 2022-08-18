3a8b3a9aca1f3e0b7521c7de5e12bbee6a6d40f2
Gordon Hicken <gordon.hicken@gmail.com>
2022-08-18 13:21:47 -0400

    Successful request from Wikipedia api

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -6,9 +6,17 @@ const Search = () => {
 
   useEffect(() => {
     const search = async () => {
-      await axios.get('url');
+      await axios.get('https://en.wikipedia.org/w/api.php', {
+        params: {
+          action: 'query',
+          list: 'search',
+          format: 'json',
+          origin: '*',
+          srsearch: term,
+        },
+      });
     };
-    
+
     search();
   }, [term]);
 
