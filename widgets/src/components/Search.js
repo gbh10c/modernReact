e934668ceeb5091bfe325c336f2452f1ebd86f17
Gordon Hicken <gordon.hicken@gmail.com>
2022-08-18 13:57:27 -0400

    Included setTimeout in Search component

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -20,15 +20,25 @@ const Search = () => {
       setResults(data.query.search);
     };
 
-    search();
+    const timeoutId = setTimeout(() => {
+      if (term) search();
+    }, 500);
   }, [term]);
 
   const renderedResults = results.map((result) => {
     return (
       <div key={result.pageid} className='item'>
+        <div className='right floated content'>
+          <a
+            className='ui button'
+            href={`https://en.wikipedia.org?curid=${result.pageid}`}
+          >
+            Go
+          </a>
+        </div>
         <div className='content'>
-          <div className='header'>{result.title};</div>
-          {result.snippet};
+          <div className='header'>{result.title}</div>
+          <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
         </div>
       </div>
     );
