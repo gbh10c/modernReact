58d7c2d3225f6253f6b52d71f4467d6dc89872df
Gordon Hicken <gordon.hicken@gmail.com>
2022-08-23 23:10:39 -0400

    Added addEventListener for dropdown to close after user clicks outside of the element on the page

--- widgets/src/components/Dropdown.js
+++ widgets/src/components/Dropdown.js
@@ -1,7 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
   const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    document.body.addEventListener(
+      'click',
+      (event) => {
+        console.log(event.target);
+        setOpen(false);
+      },
+      { capture: true }
+    );
+  }, []);
 
   const renderedOptions = options.map((option) => {
     if (option.value === selected.value) {
