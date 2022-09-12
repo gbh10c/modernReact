d4eaf67817632e97b394a8aae9154183d91fdafd
Gordon Hicken <gordon.hicken@gmail.com>
2022-09-12 10:22:46 -0400

    Included useVideos custom hook in App component

--- videos-hooks/src/components/App.js
+++ videos-hooks/src/components/App.js
@@ -1,19 +1,20 @@
 import { useState, useEffect } from 'react';
 import SearchBar from './SearchBar';
-import youtube from '../apis/youtube';
 import VideoList from './VideoList';
 import VideoDetail from './VideoDetail';
+import useVideos from '../hooks/useVideos';
 
 const App = () => {
   const [selectedVideo, setSelectedVideo] = useState(null);
+  const [videos, search] = useVideos('percussion');
 
-  // setSelectedVideo(response.data.items[0]);
-
-  
+  useEffect(() => {
+    setSelectedVideo(videos[0]);
+  }, [videos]);
 
   return (
     <div className='ui container'>
-      <SearchBar onFormSubmit={onTermSubmit} />
+      <SearchBar onFormSubmit={search} />
       <div className='ui grid'>
         <div className='ui row'>
           <div className='eleven wide column'>
