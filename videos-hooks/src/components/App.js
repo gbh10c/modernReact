b2d49f1048edc5f3a9286b3ac158119917b7b5a8
Gordon Hicken <gordon.hicken@gmail.com>
2022-09-12 10:16:03 -0400

    Created custom useVideos hook

--- videos-hooks/src/components/App.js
+++ videos-hooks/src/components/App.js
@@ -5,23 +5,11 @@ import VideoList from './VideoList';
 import VideoDetail from './VideoDetail';
 
 const App = () => {
-  const [videos, setVideos] = useState([]);
   const [selectedVideo, setSelectedVideo] = useState(null);
 
-  useEffect(() => {
-    onTermSubmit('buildings');
-  }, []);
+  // setSelectedVideo(response.data.items[0]);
 
-  const onTermSubmit = async (term) => {
-    const response = await youtube.get('/search', {
-      params: {
-        q: term,
-      },
-    });
-
-    setVideos(response.data.items);
-    setSelectedVideo(response.data.items[0]);
-  };
+  
 
   return (
     <div className='ui container'>
